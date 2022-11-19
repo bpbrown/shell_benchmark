3b770fd4252d13ea6202ee77290ed50f9bff94d1
Benjamin Brown <bpbrown@gmail.com>
2022-11-19 22:29:53 +0000

    Adds substantial outputs, including tau magnitude tracking.

--- c2001_case0.py
+++ c2001_case0.py
@@ -11,10 +11,14 @@ Options:
     --Ntheta=<Ntheta>       Latitude coeffs [default: 128]
     --Nr=<Nr>               Radial coeffs  [default: 128]
     --mesh=<mesh>           Processor mesh for 3-D runs
-"""
 
+    --max_dt=<max_dt>       Largest timestep
+
+    --label=<label>         Additional label for run output directory
+"""
+import sys
 import numpy as np
-import dedalus.public as d3
+import dedalus.public as de
 import logging
 logger = logging.getLogger(__name__)
 
@@ -44,16 +48,30 @@ Ro = 20/13
 Nr = int(args['--Nr'])
 Ntheta = int(args['--Ntheta'])
 Nphi = 2*Ntheta
-stop_sim_time = 10
+stop_sim_time = 2.5 # already converged to 1e-12 by this point #10
 
-timestepper = d3.RK222
+data_dir = sys.argv[0].split('.py')[0]
+data_dir += '_Th{}_R{}'.format(args['--Ntheta'], args['--Nr'])
+if args['--max_dt']:
+    data_dir += '_dt{}'.format(args['--max_dt'])
+
+if args['--label']:
+    data_dir += '_{:s}'.format(args['--label'])
+logger.info("saving data in {}".format(data_dir))
+
+import dedalus.tools.logging as dedalus_logging
+dedalus_logging.add_file_handler(data_dir+'/logs/dedalus_log', 'DEBUG')
+
+
+
+timestepper = de.SBDF4 #de.RK222
 dealias = 3/2
 dtype = np.float64
 
 # Bases
-coords = d3.SphericalCoordinates('phi', 'theta', 'r')
-dist = d3.Distributor(coords, dtype=dtype, mesh=mesh)
-basis = d3.ShellBasis(coords, shape=(Nphi, Ntheta, Nr), radii=(Ri, Ro), dealias=dealias, dtype=dtype)
+coords = de.SphericalCoordinates('phi', 'theta', 'r')
+dist = de.Distributor(coords, dtype=dtype, mesh=mesh)
+basis = de.ShellBasis(coords, shape=(Nphi, Ntheta, Nr), radii=(Ri, Ro), dealias=dealias, dtype=dtype)
 s2_basis = basis.S2_basis()
 V = basis.volume
 
@@ -64,11 +82,11 @@ bk2 = basis.clone_with(k=2)
 p = dist.Field(name='p', bases=bk1)
 T = dist.Field(name='T', bases=basis)
 u = dist.VectorField(coords, name='u', bases=basis)
-tau_p = dist.Field(name='tau_p')
-tau_T1 = dist.Field(name='tau_T1', bases=s2_basis)
-tau_T2 = dist.Field(name='tau_T2', bases=s2_basis)
-tau_u1 = dist.VectorField(coords, name='tau_u1', bases=s2_basis)
-tau_u2 = dist.VectorField(coords, name='tau_u2', bases=s2_basis)
+τ_p = dist.Field(name='τ_p')
+τ_T1 = dist.Field(name='τ_T1', bases=s2_basis)
+τ_T2 = dist.Field(name='τ_T2', bases=s2_basis)
+τ_u1 = dist.VectorField(coords, name='τ_u1', bases=s2_basis)
+τ_u2 = dist.VectorField(coords, name='τ_u2', bases=s2_basis)
 
 # Substitutions
 phi, theta, r = dist.local_grids(basis)
@@ -81,13 +99,13 @@ f = (2*ez/Ekman).evaluate()
 rvec = dist.VectorField(coords, bases=bk2.radial_basis)
 rvec['g'][2] = r/Ro
 
-lift = lambda A, n: d3.Lift(A, bk2, n)
+lift = lambda A, n: de.Lift(A, bk2, n)
 
 # Problem
-problem = d3.IVP([p, T, u, tau_p, tau_T1, tau_T2, tau_u1, tau_u2], namespace=locals())
-problem.add_equation("div(u) + tau_p = 0")
-problem.add_equation("dt(T) - lap(T)/Prandtl + lift(tau_T1, -1) + lift(tau_T2, -2) = -(u@grad(T))")
-problem.add_equation("dt(u) - lap(u) + grad(p)/Ekman - Rayleigh*rvec*T/Ekman + lift(tau_u1, -1) + lift(tau_u2, -2) = cross(u, curl(u) + f)")
+problem = de.IVP([p, T, u, τ_p, τ_T1, τ_T2, τ_u1, τ_u2], namespace=locals())
+problem.add_equation("div(u) + τ_p = 0")
+problem.add_equation("dt(T) - lap(T)/Prandtl + lift(τ_T1, -1) + lift(τ_T2, -2) = -(u@grad(T))")
+problem.add_equation("dt(u) - lap(u) + grad(p)/Ekman - Rayleigh*rvec*T/Ekman + lift(τ_u1, -1) + lift(τ_u2, -2) = cross(u, curl(u) + f)")
 problem.add_equation("T(r=Ri) = 1")
 problem.add_equation("u(r=Ri) = 0")
 problem.add_equation("T(r=Ro) = 0")
@@ -107,41 +125,63 @@ A = 0.1
 x = 2*r-Ri-Ro
 T['g'] = Ri*Ro/r - Ri + 210*A/np.sqrt(17920*np.pi)*(1-3*x**2+3*x**4-x**6)*np.sin(theta)**4*np.cos(4*phi)
 
-out_cadence = 1e-2
 # Analysis
-snapshots = solver.evaluator.add_file_handler('slices', sim_dt=1e-2, max_writes=10)
+out_cadence = 1e-2
+
+dot = lambda A, B: de.DotProduct(A, B)
+shellavg = lambda A: de.Average(A, coords.S2coordsys)
+volavg = lambda A: de.integ(A)/V
+
+snapshots = solver.evaluator.add_file_handler(data_dir+'/slices', sim_dt=1e-2, max_writes=10)
 snapshots.add_task(T(r=Ro), scales=dealias, name='T_r_outer')
 snapshots.add_task(T(r=Ri), scales=dealias, name='T_r_inner')
 snapshots.add_task(T(r=(Ri+Ro)/2), scales=dealias, name='T_r_mid')
 snapshots.add_task(T(phi=0), scales=dealias, name='T_phi_start')
 snapshots.add_task(T(phi=3*np.pi/2), scales=dealias, name='T_phi_end')
 
-profiles = solver.evaluator.add_file_handler('profiles', sim_dt=out_cadence, max_writes=100)
+profiles = solver.evaluator.add_file_handler(data_dir+'/profiles', sim_dt=out_cadence, max_writes=100)
 profiles.add_task(u(r=(Ri+Ro)/2,theta=np.pi/2), name='u_profile')
 profiles.add_task(T(r=(Ri+Ro)/2,theta=np.pi/2), name='T_profile')
 
-traces = solver.evaluator.add_file_handler('traces', sim_dt=out_cadence, max_writes=100)
-traces.add_task(0.5*d3.integ(u@u)/V, name='KE')
+traces = solver.evaluator.add_file_handler(data_dir+'/traces', sim_dt=out_cadence, max_writes=None)
+traces.add_task(0.5*volavg(u@u), name='KE')
+traces.add_task(np.abs(τ_p), name='τ_p')
+traces.add_task(shellavg(np.abs(τ_T1)), name='τ_T1')
+traces.add_task(shellavg(np.abs(τ_T2)), name='τ_T2')
+traces.add_task(shellavg(np.sqrt(dot(τ_u1,τ_u1))), name='τ_u1')
+traces.add_task(shellavg(np.sqrt(dot(τ_u1,τ_u1))), name='τ_u2')
 
 # CFL
-max_timestep = 1e-4
-CFL = d3.CFL(solver, initial_dt=max_timestep, cadence=1, safety=0.35, threshold=0.1,
+if args['--max_dt']:
+    max_timestep = float(args['--max_dt'])
+else:
+    max_timestep = Ekman/10
+
+CFL = de.CFL(solver, initial_dt=max_timestep, cadence=1, safety=0.35, threshold=0.1,
              max_change=1.5, min_change=0.5, max_dt=max_timestep)
 CFL.add_velocity(u)
 
 # Flow properties
-flow = d3.GlobalFlowProperty(solver, cadence=10)
+flow = de.GlobalFlowProperty(solver, cadence=10)
 flow.add_property(np.sqrt(u@u), name='Re')
+flow.add_property(np.abs(τ_p), name='|τ_p|')
+flow.add_property(np.abs(τ_T1), name='|τ_T1|')
+flow.add_property(np.abs(τ_T2), name='|τ_T2|')
+flow.add_property(np.sqrt(dot(τ_u1,τ_u1)), name='|τ_u1|')
+flow.add_property(np.sqrt(dot(τ_u2,τ_u2)), name='|τ_u2|')
 
+report_cadence = 10
 # Main loop
 try:
     logger.info('Starting main loop')
     while solver.proceed:
-        timestep = CFL.compute_timestep()
-        solver.step(timestep)
-        if (solver.iteration-1) % 10 == 0:
+        Δt = CFL.compute_timestep()
+        solver.step(Δt)
+        if solver.iteration > 0 and solver.iteration % report_cadence == 0:
             max_Re = flow.max('Re')
-            logger.info('Iteration=%i, Time=%e, dt=%e, max(Re)=%f' %(solver.iteration, solver.sim_time, timestep, max_Re))
+            max_τ = np.max([flow.max('|τ_u1|'), flow.max('|τ_u2|'), flow.max('|τ_T1|'), flow.max('|τ_T2|'), flow.max('|τ_p|')])
+
+            logger.info('Iteration={:d}, Time={:e}, dt={:e}, max(Re)={:.3g}, τ={:.2g}'.format(solver.iteration, solver.sim_time, Δt, max_Re, max_τ))
 except:
     logger.error('Exception raised, triggering end of main loop.')
     raise
---
+++ plot_scalar.py
@@ -0,0 +1,124 @@
+"""
+Plot scalar outputs from scalar_output.h5 file.
+
+Usage:
+    plot_scalar.py <file> [options]
+
+Options:
+    --times=<times>      Range of times to plot over; pass as a comma separated list with t_min,t_max.  Default is whole timespan.
+    --output=<output>    Output directory; if blank, a guess based on <file> location will be made.
+"""
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pathlib
+import h5py
+
+import logging
+logger = logging.getLogger(__name__.split('.')[-1])
+
+from docopt import docopt
+args = docopt(__doc__)
+file = args['<file>']
+
+if args['--output'] is not None:
+    output_path = pathlib.Path(args['--output']).absolute()
+else:
+    data_dir = args['<file>'].split('/')[0]
+    data_dir += '/'
+    output_path = pathlib.Path(data_dir).absolute()
+
+f = h5py.File(file, 'r')
+data = {}
+t = f['scales/sim_time'][:]
+data_slice = (slice(None),0,0,0)
+for key in f['tasks']:
+    data[key] = f['tasks/'+key][data_slice]
+f.close()
+
+if args['--times']:
+    subrange = True
+    t_min, t_max = args['--times'].split(',')
+    t_min = float(t_min)
+    t_max = float(t_max)
+    print("plotting over range {:g}--{:g}, data range {:g}--{:g}".format(t_min, t_max, min(t), max(t)))
+else:
+    subrange = False
+
+fig_E, ax_E = plt.subplots(nrows=2)
+for key in ['KE']:
+    ax_E[0].plot(t, data[key], label=key)
+    ax_E[1].plot(t, np.abs(data[key]-data[key][-1]), label=key)
+
+for ax in ax_E:
+    if subrange:
+        ax.set_xlim(t_min,t_max)
+    ax.set_xlabel('time')
+    ax.set_ylabel('energy density')
+    ax.legend(loc='lower left')
+fig_E.savefig('{:s}/energies.pdf'.format(str(output_path)))
+for ax in ax_E:
+    ax.set_yscale('log')
+fig_E.savefig('{:s}/log_energies.pdf'.format(str(output_path)))
+
+fig_tau, ax_tau = plt.subplots(nrows=2)
+for i in range(2):
+    ax_tau[i].plot(t, data['τ_u1'], label=r'$\tau_{u1}$')
+    ax_tau[i].plot(t, data['τ_u2'], label=r'$\tau_{u2}$')
+    ax_tau[i].plot(t, data['τ_T1'], label=r'$\tau_{T1}$')
+    ax_tau[i].plot(t, data['τ_T2'], label=r'$\tau_{T2}$')
+    ax_tau[i].plot(t, data['τ_p'], label=r'$\tau_{p}$')
+
+for ax in ax_tau:
+    if subrange:
+        ax.set_xlim(t_min,t_max)
+    ax.set_xlabel('time')
+    ax.set_ylabel(r'$<\tau>$')
+    ax.legend(loc='lower left')
+ax_tau[1].set_yscale('log')
+ylims = ax_tau[1].get_ylim()
+ax_tau[1].set_ylim(max(1e-14, ylims[0]), ylims[1])
+fig_tau.savefig('{:s}/tau_error.pdf'.format(str(output_path)))
+
+#
+# fig_L, ax_L = plt.subplots(nrows=2)
+# ax_L[0].plot(t, data['Lz'], label='Lz')
+# ax_L[1].plot(t, np.abs(data['Lz']), label='Lz')
+#
+# for ax in ax_L:
+#     if subrange:
+#         ax.set_xlim(t_min,t_max)
+#     ax.set_xlabel('time')
+#     ax.set_ylabel('Angular Momentum')
+#     ax.legend(loc='lower left')
+# ax_L[1].set_yscale('log')
+# fig_L.savefig('{:s}/angular_momentum.pdf'.format(str(output_path)))
+#
+# fig_f, ax_f = plt.subplots(nrows=2)
+# for ax in ax_f:
+#     ax.plot(t, data['Re'], label='Re')
+#     ax_r = ax.twinx()
+#     ax_r.plot(t, data['Ro'], label='Ro', color='tab:orange')
+#     if subrange:
+#         ax.set_xlim(t_min,t_max)
+#     ax.set_xlabel('time')
+#     ax.set_ylabel('fluid parameters')
+#     ax.legend(loc='lower left')
+#
+# ax_f[1].set_yscale('log')
+# ax_r.set_yscale('log') # relies on it being the last instance; poor practice
+#
+# fig_f.savefig('{:s}/Re_and_Ro.pdf'.format(str(output_path)))
+
+benchmark_set = ['KE']
+
+i_ten = int(0.9*data[benchmark_set[0]].shape[0])
+print("benchmark values")
+for benchmark in benchmark_set:
+    print("{:s} = {:14.12g} +- {:4.2g} (averaged from {:g}-{:g})".format(benchmark, np.mean(data[benchmark][i_ten:]), np.std(data[benchmark][i_ten:]), t[i_ten], t[-1]))
+print()
+for benchmark in benchmark_set:
+    print("{:s} = {:14.12g} (at t={:g})".format(benchmark, data[benchmark][-1], t[-1]))
+print("total simulation time {:6.2g}".format(t[-1]-t[0]))
