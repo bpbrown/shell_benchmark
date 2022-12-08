73cff3a1013a074d8116773cb0c7527a81c4369b
Benjamin Brown <bpbrown@gmail.com>
2022-12-08 15:57:49 +0000

    Generalizes ncc plotting to handle both major benchmark cases.

--- plot_L_cons_ncc.py
+++ plot_L_cons_ncc.py
@@ -1,12 +1,18 @@
 """
 Dedalus script for testing ncc expansion sizes of angular-momentum conserving weight function for shells.
 
+currently implemented benchmarks are:
+    c2000       Christensen et al 2000, case 0, Boussinesq
+    j2011       Jones et al 2011, hydro case, anelastic
+
 Usage:
     plot_L_cons_ncc.py [options]
 
 Options:
-    --Nr=<Nr>               Radial coeffs  [default: 128]
-    --ncc_cutoff=<ncc_cutoff>            Amplitude to truncate NCC terms [default: 1e-10]
+    --benchmark=<bench>  Benchmark to test [default: c2000]
+
+    --Nr=<Nr>            Radial coeffs  [default: 128]
+    --ncc_cutoff=<ncc>   Amplitude to truncate NCC terms [default: 1e-10]
 """
 import numpy as np
 import dedalus.public as de
@@ -14,10 +20,19 @@ import dedalus.public as de
 dealias = 3/2
 dtype = np.float64
 
-Ri = 7/13
-Ro = 20/13
+def calculate_ncc(Nr, benchmark='c2000'):
+    if benchmark == 'c2000':
+        # Christensen et al. 2000, case 0 benchmark
+        Ri = 7/13
+        Ro = 20/13
+    elif benchmark == 'j2011':
+        # Jones et al. benchmark, hydro
+        beta = 0.35
+        Ro = 1/(1-beta)
+        Ri = Ro - 1
+    else:
+        raise ValueError("benchmark={} is not either 'c2000' or 'j2011'".format(benchmark))
 
-def calculate_ncc(Nr):
     Ntheta = 4
     Nphi = 2*Ntheta
 
@@ -26,14 +41,34 @@ def calculate_ncc(Nr):
     dist = de.Distributor(coords, dtype=dtype)
     basis = de.ShellBasis(coords, shape=(Nphi, Ntheta, Nr), radii=(Ri, Ro), dealias=dealias, dtype=dtype)
 
+    phi, theta, r = dist.local_grids(basis)
+
     b_ncc = de.ShellBasis(coords, shape=(1, 1, Nr), radii=(Ri, Ro), dealias=dealias, dtype=dtype)
     #b_ncc = basis.radial_basis
+    ρ = dist.Field(bases=b_ncc, name='ρ')
 
-    phi, theta, r = dist.local_grids(basis)
+    if benchmark == 'c2000':
+        ρ['g'] = 1
+
+    elif benchmark == 'j2011':
+        n = 2
+        Nrho = 5
+
+        zeta_out = (beta + 1) / ( beta*np.exp(Nrho/n) + 1 )
+        zeta_in  = (1 + beta - zeta_out) / beta
+        c0 = (2*zeta_out - beta - 1) / (1 - beta)
+        c1 = (1 + beta)*(1 - zeta_out) / (1 - beta)**2
+
+        zeta = dist.Field(bases=b_ncc, name='zeta')
+        zeta['g'] = c0 + c1/r
+        ρ.change_scales(dealias)
+        ρ['g'] = (zeta**n).evaluate()['g']
+        ρ.change_scales(1)
 
     L_cons_ncc = dist.Field(bases=b_ncc, name='L_cons_ncc')
     R_avg = (Ro+Ri)/2
-    L_cons_ncc['g'] = (r/R_avg)**3*np.sqrt((r/Ro-1)*(1-r/Ri))
+    L_cons_ncc['g'] = ρ['g']*(r/R_avg)**3*np.sqrt((r/Ro-1)*(1-r/Ri))
+
     return L_cons_ncc
 
 if __name__=='__main__':
@@ -47,6 +82,8 @@ if __name__=='__main__':
     from docopt import docopt
     args = docopt(__doc__)
 
+    benchmark = args['--benchmark']
+
     Nr_min = 16
     Nr_max = int(args['--Nr'])
 
@@ -59,7 +96,7 @@ if __name__=='__main__':
 
     L_ncc_set = []
     for Nr in Nr_set:
-        L_ncc_set.append(calculate_ncc(Nr))
+        L_ncc_set.append(calculate_ncc(Nr, benchmark=benchmark))
 
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots()
@@ -78,4 +115,4 @@ if __name__=='__main__':
     ax.set_yscale('log')
     ax.set_xscale('log')
     ax.legend()
-    fig.savefig('L_cons_ncc_Nr{:d}-{:d}.png'.format(Nr_min,Nr_max), dpi=300)
+    fig.savefig('L_cons_ncc_{}_Nr{:d}-{:d}.png'.format(benchmark, Nr_min,Nr_max), dpi=300)
